116535818ffbfbfd0101f8aefbb7b0cd8ca2b320
Martin Avendaño <martinavendano205@gmail.com>
2025-11-23 15:58:05 +0800

    Updated Dashboard V2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
 
     form.addEventListener("submit", function(e) {
-        e.preventDefault(); // stop default refresh
+        e.preventDefault();
 
         console.log("Form submitted! Redirecting...");
 
